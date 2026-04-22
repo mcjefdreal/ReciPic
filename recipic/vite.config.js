75282f04902d879e619aed82ad36597686460119
RpnAndri <andri.crisostomo@gmail.com>
2026-04-22 22:52:58 +0800

    initial pwa

---
+++ recipic/vite.config.js
@@ -0,0 +1,34 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+	plugins: [
+    sveltekit(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'ReciPic',
+        short_name: 'ReciPic',
+        description: 'Image to Recipe',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icons/sample.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/sample.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+	
+});
